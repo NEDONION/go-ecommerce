6c9d62db093ba5ecc3b690406c624dc85341c63c
NEDONION <nedjiachenghu@gmail.com>
2023-03-29 15:42:33 -0500

    feat: create models in model layer

---
+++ model/order.go
@@ -0,0 +1,15 @@
+package model
+
+import "gorm.io/gorm"
+
+type Order struct {
+	gorm.Model
+	UserID    uint   `gorm:"not null"`
+	ProductID uint   `gorm:"not null"`
+	BossID    uint   `gorm:"not null"`
+	AddressID uint   `gorm:"not null"`
+	Num       int    // 数量
+	OrderNum  uint64 // 订单号
+	Type      uint   // 1 未支付  2 已支付
+	Money     float64
+}
